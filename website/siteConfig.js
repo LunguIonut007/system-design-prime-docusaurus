b182c764f3e208817665f0cd41cfd4dda24a7013
Adrian Ioan Lungu <adrian.lungu@gentlab.com>
2020-10-02 20:37:45 +0300

    moved docs folder

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,8 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'test-site',
-  organizationName: 'facebook',
+  customDocsPath: './website/docs',
+  organizationName: 'lunguionut',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
